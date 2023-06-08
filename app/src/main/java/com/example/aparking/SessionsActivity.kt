871b59aea3b202a3d74f8e3ba47f2178ad6bab56
mrGnost <mrgnost@yandex.ru>
2023-06-08 16:32:59 +0300

    Sessions screens navigation added

--- app/src/main/java/com/example/aparking/SessionsActivity.kt
+++ app/src/main/java/com/example/aparking/SessionsActivity.kt
@@ -2,12 +2,15 @@ package com.example.aparking
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.aparking.databinding.ActivitySessionsBinding
 
 class SessionsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySessionsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sessions)
+        binding = ActivitySessionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Прячем верхнюю панель (ActionBar)
         supportActionBar?.hide()
@@ -16,5 +19,26 @@ class SessionsActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.fragment_container, CurrentSessionFragment(), "CurrentSessionFragment")
             .commit()
+
+        binding.currentButton.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, CurrentSessionFragment(), "CurrentSessionFragment")
+                .commit()
+        }
+
+        binding.unpaidButton.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, UnpaidSessionFragment(), "UnpaidSessionFragment")
+                .commit()
+        }
+
+        binding.historyButton.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, HistorySessionFragment(), "HistorySessionFragment")
+                .commit()
+        }
     }
 }
